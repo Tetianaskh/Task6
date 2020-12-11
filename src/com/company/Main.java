4166b7b08daa80ef456ea3e853badd9bb1d8cc7a
Tetiana Serdiuk <tetianaskh@gmail.com>
2020-12-11 11:30:31 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    // Основные задачи:
+
+    // Задача 1. Удаление элемента по индексу
+    public static void exercise01() {
+
+    }
+
+    // Задача 2. Склейки двух массивов
+    public static void exercise02() {
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
