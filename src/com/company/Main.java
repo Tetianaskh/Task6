3d2005b8c52ace910b423d9a0d4649372351fb44
Tetiana Serdiuk <tetianaskh@gmail.com>
2020-12-14 18:07:55 +0200

    Основные задачи:

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,28 @@
 package com.company;
 
+import java.sql.Array;
+
 public class Main {
 
     // Основные задачи:
 
-    // Задача 1. Удаление элемента по индексу
+    public static void removingArrayElement (String[] Array, int index) {
+        for () {
+
+        }
+    }
+
+    /**
+     * Задача 1. Удаление элемента по индексу
+     */
     public static void exercise01() {
+
 
     }
 
-    // Задача 2. Склейки двух массивов
+    /**
+     * Задача 2. Склейки двух массивов
+     */
     public static void exercise02() {
 
     }
